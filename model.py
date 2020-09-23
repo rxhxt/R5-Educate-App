91e462b618dd65a8271959a22a3ce89e9f357df2
jineshparakh <jineshparakh@hotmail.com>
2020-09-23 10:44:40 +0530

    Summarization

--- model.py
+++ model.py
@@ -20,13 +20,14 @@ def getSummary(text,tokenizer):
     tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
 
     summary_ids = model.generate(tokenized_text,
-                                        num_beams=5,
+                                        num_beams=4,
                                         no_repeat_ngram_size=2,
                                         min_length=30,
-                                        max_length=96,
+                                        max_length=100,
                                         early_stopping=True)
 
     output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    print(output)
     return output
 
 def sentenceCorrection(text):
@@ -62,7 +63,7 @@ def summaryGeneration(mailid):
                 textTokens = word_tokenize(text)
                 totalTokens = len(textTokens)
                 chunkCounter = 0
-                maxTokenLen = 400
+                maxTokenLen = 1000
                 chunkList = []
                 start = 0
                 end = maxTokenLen
@@ -95,6 +96,7 @@ def summaryGeneration(mailid):
 
                 for chunk in chunkList:
                     tempSummary = getSummary(chunk, tokenizer)
+                    print(tempSummary)
                     summary += tempSummary
 
                 summary = sentenceCorrection(summary)
