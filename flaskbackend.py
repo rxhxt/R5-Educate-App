48a364ac6e631a8ffbcc22b56620e510f24b4bed
jineshparakh <jineshparakh@hotmail.com>
2020-09-25 11:48:21 +0530

    Update Notifications for teachers and profile for students

--- flaskbackend.py
+++ flaskbackend.py
@@ -356,7 +356,10 @@ def test_page2(q_id):
 
 
 
-
+@app.route("/profile_student", methods=['GET', 'POST'])
+@login_required
+def profile_student():
+    return render_template('profile_student.html', user=current_user)
 
 def allowed_pdf(filename):
     if not "." in filename:
@@ -377,6 +380,18 @@ def notifications_student():
         l=[upload.name, upload.time_uploaded, upload.message,uu.name]
         uploads.append(l)
     return render_template("notification_student.html", uploads=uploads)
+
+@app.route("/notification-teacher", methods=['GET'])
+@login_required
+def notifications_teacher():
+    summaries=[]
+    s=Summarizer.query.all()
+    for summary in s:
+        by=summary.user_id
+        uu=User.query.filter_by(id=by).first()
+        l=[summary.email, summary.time_when_requested, summary.file_name,uu.name]
+        summaries.append(l)
+    return render_template("notification_teacher.html", summaries=summaries)    
 
 @app.route('/download', methods=['GET', 'POST'])
 @login_required
